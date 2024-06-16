b7ff9fbad16f16a38287a32607a85f9de0ba4d13
Rupoon <newaone@gmail.com>
2024-06-16 09:45:13 +0600

    adding all file together

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors:{
+        'bg-navcolors': '#2B2B2B',
+        'bg-home1': '#474747',
+        'bg-home2': '#222222',
+        'discover1': '#60BBEE',
+        'discover2': '#0A72AD',
+        'afterhome1': '#399FD8',
+        'afterhome2': '#0775B4',
+        'afterhome3': '#003756',
+      },
+    },
+  },
+  plugins: [],
+}
+
